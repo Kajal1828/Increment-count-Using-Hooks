9b0c20553ca451c25dc85fdb35c079d7f9256a74
kajal <kajalneha.nk@gmail.com>
2023-01-07 00:48:31 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import { useState } from 'react';
+
+const App = () => {
+
+const state = useState();
+const[count, setcount] = useState(0);
+
+const IncNum = () => {
+    setcount(count + 1);
+
+}
+  return (
+    <>
+    <h1>{count}</h1>
+    <button onClick={IncNum}>Click Me</button>
+    </>
+  );
+};
+
+export default App
+
